16f03b735c942ab33d02f253677821d3b7be7987
rajveer1k <rajveersinghkalsi2008@gmail.com>
2026-06-11 11:29:00 -0500

    Create drop.js

---
+++ sketch/drop.js
@@ -0,0 +1,25 @@
+function Drop(x, y){
+  this.x = x;
+  this.y = y;
+  this.r = 8;
+  this.toDelete = false;
+
+  this.show = function(){
+    noStroke();
+    fill(150, 0, 255);
+    ellipse(this.x, this.y, this.r*2, this.r*2);
+  }
+
+  this.evaporate = function(){
+    this.toDelete = true;
+  }
+
+  this.hits = function(flower){
+    var d = dist(this.x, this.y, flower.x, flower.y);
+    return d < this.r + flower.r;
+  }
+
+  this.move = function(){
+    this.y -= 5;
+  }
+}
